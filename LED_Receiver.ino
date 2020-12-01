156461bb85834e5b8af72d305ef3f9008a9f52fc
Carl Friess <carl.friess@me.com>
2020-12-01 17:30:28 +0100

    Implement RS232-style protocol

--- LED_Receiver.ino
+++ LED_Receiver.ino
@@ -1,6 +1,12 @@
 #define LED_A A1  // +
 #define LED_C A0  // -
 
+#define LPF_M 6
+
+#define SIGNAL_THRESHOLD  40
+#define SYMBOL_PERIOD     5   // 100Hz / 20Hz = 5
+#define SAMPLE_OFFSET     2
+
 void setup() {
 
   // Start out with LED pins as output
@@ -57,11 +63,97 @@ ISR(TIMER0_COMPA_vect) {
 
 void loop() {
 
+  static uint16_t lpf;
+  static boolean start_detected = false;
+  static int counter;
+  static int bit_pos;
+  static char recv_char;
+
   // Check if the flag was set
   // Should happen at approx. 100Hz
   if (flag) {
 
-    Serial.println(light_value);
+    // Low-pass filter
+    lpf = lpf - (lpf >> LPF_M) + light_value;
+
+    // Evaluate measurement
+    uint16_t lpf_value = lpf >> LPF_M;
+    boolean symbol = light_value > lpf_value + SIGNAL_THRESHOLD;
+
+    // Check for start bit
+    if (!start_detected && symbol) {
+
+      // Start symbol timing counter
+      counter = -SAMPLE_OFFSET - 1;
+
+      // Initialise parser
+      start_detected = true;
+      bit_pos = 8;
+      recv_char = 0;
+      
+    }
+
+    // Check if we are currently reading a byte
+    if (start_detected) {
+
+      // Increment symbol timing counter
+      if (++counter == SYMBOL_PERIOD) {
+        counter = 0;
+      }
+
+      // Check if we are at the sampling point
+      if (counter == 0) {
+
+        if (bit_pos == 8) {
+          
+          // If this is the start bit, it must be set
+          if (!symbol) {
+            start_detected = false;
+          }
+          
+        } else if (bit_pos == -1) {
+
+          // Calculate and check parity
+          char parity = recv_char ^ (recv_char >> 1);
+          parity = parity ^ (parity >> 2);
+          parity = parity ^ (parity >> 4);
+          if (parity & 1 != !!symbol) {
+            start_detected = false;
+          }
+          
+        } else if (bit_pos == -2) {
+
+          // If this is the stop bit, it must be clear
+          if (!symbol) {
+            // Received a good character
+          }
+
+          // Finish receiving
+          start_detected = false;
+          
+        } else {
+
+          recv_char |= !!symbol << bit_pos;
+          
+        }
+
+        // Decrement the sampled bit position
+        bit_pos--;
+        
+      }
+            
+    }
+
+    // Print data
+    Serial.print(lpf_value);
+    Serial.print("\t");
+    Serial.print(symbol ? 400 : 0);
+    Serial.print("\t");
+    Serial.print(light_value);
+    Serial.print("\t");
+    Serial.print((unsigned char) recv_char, HEX);
+    Serial.print(":");
+    Serial.println(start_detected ? 400 : 0);
 
     // Clear flag
     flag = false;
